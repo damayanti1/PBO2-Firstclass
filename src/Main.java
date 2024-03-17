479ce4ad8854c7d2a45b76fe5814b278216ffc20
unknown <damayy1404@gmail.com>
2024-03-17 19:59:56 +0700

    firstClass

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+         System.out.println("Hello My Name Damayanti Eka Putri");
+        //
+         System.out.println("My Age 19");
+         System.out.println("Ini adalah branch development");
+
+        boolean isAlien = false;
+
+        if (isAlien == false) { /*!isAlien*/
+            System.out.println("im scared of alien");
+            System.out.println("its not an alien!!");
+        }
+
+
+        System.out.println("Who i a'm?");
+
+        }
+    }
